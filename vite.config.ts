988acca6f68761d90e1083f8ce0538707c635763
0xDragonDev <edward.dragon.x@gmail.com>
2023-06-10 11:56:36 -0300

    feat: change build dir

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'docs'
+  },
   resolve: {
     alias: {
       components: path.resolve(__dirname, './src/components'),
